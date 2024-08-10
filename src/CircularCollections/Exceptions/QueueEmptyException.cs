ff46a63df2e2b2b188605a970cadd2a5d17ebe65
Kris Craig <Kris.Craig@gmail.com>
2024-08-10 03:59:59 -0700

    Exception consistency.

---
+++ src/CircularCollections/Exceptions/QueueEmptyException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Collections.Generic.Circular.Exceptions
+{
+    /// <summary>
+    /// Exception that is thrown when attempting to access from an empty heap.
+    /// </summary>
+    [Serializable]
+    public class QueueEmptyException : Exception
+    {
+        public QueueEmptyException(string message, Exception inner)
+            : base(message, inner) { }
+
+        public QueueEmptyException(string message)
+            : base(message) { }
+
+        public QueueEmptyException()
+            : base() { }
+
+        protected QueueEmptyException(SerializationInfo serializationInfo, StreamingContext context)
+            : base(serializationInfo, context) { }
+    }
+}
---
+++ src/CircularCollections/Exceptions/StackEmptyException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Collections.Generic.Circular.Exceptions
+{
+    /// <summary>
+    /// Exception that is thrown when attempting to access from an empty heap.
+    /// </summary>
+    [Serializable]
+    public class StackEmptyException : Exception
+    {
+        public StackEmptyException(string message, Exception inner)
+            : base(message, inner) { }
+
+        public StackEmptyException(string message)
+            : base(message) { }
+
+        public StackEmptyException()
+            : base() { }
+
+        protected StackEmptyException(SerializationInfo serializationInfo, StreamingContext context)
+            : base(serializationInfo, context) { }
+    }
+}
